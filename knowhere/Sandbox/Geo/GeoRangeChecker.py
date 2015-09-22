b98cdd3724bba3df725b1055da6aa630a1c80fd7
Adam <adam12_81@yahoo.com>
2015-09-22 06:59:30 -0500

    Initial commit for geo experimentations - and the rest that I missed

---
+++ knowhere/Sandbox/Geo/GeoRangeChecker.py
@@ -0,0 +1,16 @@
+__author__ = 'Adam'
+
+import Location
+
+origin = Location(0.0, 0.0)
+destination = Location(0.0, 0.0)
+
+def setOrigin(_origin ):
+    origin =
+    originLon = lon
+
+def setDestination(lat, lon):
+    desLat = lat
+    desLon = lon
+
+def calcDistance():
