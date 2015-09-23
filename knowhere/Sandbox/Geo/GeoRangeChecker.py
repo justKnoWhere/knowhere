d51b255f19df4bf74fe4b24e4f8f20e2f6e51808
athornton <adam12_81@yahoo.com>
2015-09-23 16:59:54 -0500

    Updated GeoRangeChecker

--- knowhere/Sandbox/Geo/GeoRangeChecker.py
+++ knowhere/Sandbox/Geo/GeoRangeChecker.py
@@ -1,16 +1,31 @@
 __author__ = 'Adam'
 
-import Location
+import math
 
-origin = Location(0.0, 0.0)
-destination = Location(0.0, 0.0)
+kmInMile = 0.62137119
 
-def setOrigin(_origin ):
-    origin =
-    originLon = lon
 
-def setDestination(lat, lon):
-    desLat = lat
-    desLon = lon
+def isInRange(range, originLat, originLon, destinationLat, destinationLon):
+    return haversine(originLat, originLon, destinationLat, destinationLon) <= range
 
-def calcDistance():
+
+def getDistanceKm(originLat, originLon, destinationLat, destinationLon):
+    return haversine(originLat, originLon, destinationLat, destinationLon)
+
+
+def getDistanceMi(originLat, originLon, destinationLat, destinationLon):
+    return haversine(originLat, originLon, destinationLat, destinationLon) * kmInMile
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6372.8  # Earth radius in kilometers
+
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    a = sin(dLat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dLon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+
+    return R * c
