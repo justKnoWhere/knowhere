a6b69ee1fb0356bd82149e136969e7849ddb7901
Adam <adam12_81@yahoo.com>
2015-09-27 08:35:18 -0500

    Initial commit for new notification model, test model, and html

--- website/models.py
+++ website/models.py
@@ -8,3 +8,15 @@ class User(models.Model):
     email_address = models.CharField(max_length=254)
     password = models.CharField(max_length=64)
     salt = models.CharField(max_length=64)
+
+
+class Notification(models.Model):
+    id = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    notification_group = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=254)
+    state = models.CharField(max_length=64)
+    zipcode = models.CharField(max_length=64)
+    date = models.DateField(max_length=64)
+    time = models.DateTimeField(max_length=64)
--- website/tests/test_models.py
+++ website/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from website.models import User
+from website.models import User, Notification
 
 
 class UserTest(TestCase):
@@ -34,3 +34,44 @@ class UserTest(TestCase):
         self.assertEqual(self.user.email_address, self.EMAIL_ADDRESS)
         self.assertEqual(self.user.password, self.PASSWORD)
         self.assertEqual(self.user.salt, self.SALT)
+
+
+class NotificationTest(TestCase):
+    ID = "1234ABCD"
+    NAME = "My Notification"
+    NOTIFICATION_GROUP = "My Group"
+    ADDRESS = "123 Main St."
+    CITY = "San Antonio"
+    STATE = "TX"
+    ZIPCODE = "12345"
+    DATE = "Jan 1, 1970"
+    TIME = "12:00 AM"
+
+    notification = Notification()
+
+    def test_user_creation(self):
+        self.given_a_new_notification_is_created()
+        self.then_the_notification_has_expected_values()
+
+    def given_a_new_notification_is_created(self):
+        self.notification = Notification.objects.create(
+            id=self.ID,
+            name=self.NAME,
+            address=self.ADDRESS,
+            city=self.CITY,
+            state=self.STATE,
+            zipcode=self.zipcode,
+            date=self.DATE,
+            time=self.TIME
+        )
+
+    def then_the_user_has_expected_values(self):
+        self.assertTrue(isinstance(self.notification, Notification))
+        self.assertEqual(self.notification.id, self.ID)
+        self.assertEqual(self.notification.name, self.NAME)
+        self.assertEqual(self.notification.address, self.ADDRESS)
+        self.assertEqual(self.notification.city, self.CITY)
+        self.assertEqual(self.notification.state, self.STATE)
+        self.assertEqual(self.notification.zipcode, self.ZIPCODE)
+        self.assertEqual(self.notification.date, self.DATE)
+        self.assertEqual(self.notification.time, self.TIME)
