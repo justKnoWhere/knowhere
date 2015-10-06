325410fa813c32eae8bfeb8e05a66ede797f9dcc
KingStrafeIV <bz.kalashnikov@gmail.com>
2015-10-06 10:44:06 -0500

    MAINT Fix bugs and update relevant tests

--- website/models.py
+++ website/models.py
@@ -10,21 +10,20 @@ class User(models.Model):
     salt = models.CharField(max_length=64)
 
 
+class Group(models.Model):
+    users = models.ManyToManyField(User)
+    name = models.CharField(max_length=64)
+
+
 class Notification(models.Model):
     user = models.ForeignKey(User)
-    name = models.CharField(max_length=64)
-    notification_groups = models.ManyToManyField(Group)
+    title = models.CharField(max_length=64)
+    groups = models.ManyToManyField(Group)
     address = models.CharField(max_length=64)
     city = models.CharField(max_length=254)
     state = models.CharField(max_length=64)
     zipcode = models.CharField(max_length=64)
-    date = models.DateField(max_length=64)
-    time = models.DateTimeField(max_length=64)
+    date = models.DateField()
+    time = models.TimeField()
 
 
-class Group(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=64)
-    email = models.CharField(max_lengh=64)
-    emailList = models.CharField(max_length=2048)
-    #Maybe convert the database entry into JSON string; I'm not really sure how to back a select/combo box
--- website/tests/test_models.py
+++ website/tests/test_models.py
@@ -36,14 +36,42 @@ class UserTest(TestCase):
         self.assertEqual(self.user.salt, self.SALT)
 
 
+class GroupTest(TestCase):
+    NAME = "Test Group"
+    USERNAME_1 = "user1"
+    USERNAME_2 = "user2"
+
+    group = Group()
+
+    def test_group_creation(self):
+        self.given_group_is_created()
+        self.then_the_group_has_expected_values()
+
+    def given_group_is_created(self):
+        user1 = User.objects.create(username=self.USERNAME_1)
+        user2 = User.objects.create(username=self.USERNAME_2)
+
+        self.group = Group.objects.create(name=self.NAME)
+        self.group.users.add(user1, user2)
+
+    def then_the_group_has_expected_values(self):
+        group_users = self.group.users.all()
+        self.assertTrue(isinstance(self.group, Group))
+        self.assertEqual(self.group.name, self.NAME)
+        self.assertEqual(group_users[0].username, self.USERNAME_1)
+        self.assertEqual(group_users[1].username, self.USERNAME_2)
+
+
 class NotificationTest(TestCase):
-    NAME = "My Notification"
-    NOTIFICATION_GROUP = "My Group"
+    USERNAME = "notificationUser"
+    TITLE = "My Notification"
+    GROUP_NAME_1 = "My Group 1"
+    GROUP_NAME_2 = "My Group 2"
     ADDRESS = "123 Main St."
     CITY = "San Antonio"
     STATE = "TX"
     ZIPCODE = "12345"
-    DATE = "Jan 1, 1970"
+    DATE = "1970-01-01"
     TIME = "12:00 AM"
 
     notification = Notification()
@@ -53,47 +81,32 @@ class NotificationTest(TestCase):
         self.then_the_notification_has_expected_values()
 
     def given_the_notification_is_created(self):
+        user = User.objects.create(username=self.USERNAME)
+        group1 = Group.objects.create(name=self.GROUP_NAME_1)
+        group2 = Group.objects.create(name=self.GROUP_NAME_2)
+
         self.notification = Notification.objects.create(
-            name=self.NAME,
+            user=user,
+            title=self.TITLE,
             address=self.ADDRESS,
             city=self.CITY,
             state=self.STATE,
-            zipcode=self.zipcode,
+            zipcode=self.ZIPCODE,
             date=self.DATE,
             time=self.TIME
         )
+        self.notification.groups.add(group1, group2)
 
     def then_the_notification_has_expected_values(self):
+        notification_groups = self.notification.groups.all()
         self.assertTrue(isinstance(self.notification, Notification))
-        self.assertEqual(self.notification.name, self.NAME)
+        self.assertEqual(self.notification.user.username, self.USERNAME)
+        self.assertEqual(notification_groups[0].name, self.GROUP_NAME_1)
+        self.assertEqual(notification_groups[1].name, self.GROUP_NAME_2)
+        self.assertEqual(self.notification.title, self.TITLE)
         self.assertEqual(self.notification.address, self.ADDRESS)
         self.assertEqual(self.notification.city, self.CITY)
         self.assertEqual(self.notification.state, self.STATE)
         self.assertEqual(self.notification.zipcode, self.ZIPCODE)
         self.assertEqual(self.notification.date, self.DATE)
         self.assertEqual(self.notification.time, self.TIME)
-
-
-class NotifcationGroupTest(TestCase):
-    NAME = "GROUP TEST"
-    EMAIL = "test@email.com"
-    EMAIL_LIST = "{\"emailList\":[{\"email\":\"email1@email.com\"},{\"email2\":\"email1@email.com\"}]}"
-
-    group = Group()
-
-    def test_group_creation(self):
-        self.given_group_is_created()
-        self.then_the_group_has_expected_values()
-
-    def given_group_is_created(self):
-        self.notification = Notification.objects.create(
-            name=self.NAME,
-            email=self.EMAIL,
-            address=self.EMAIL_LIST
-        )
-
-    def then_the_group_has_expected_values(self):
-        self.assertTrue(isinstance(self.notification, Notification))
-        self.assertEqual(self.notification.name, self.NAME)
-        self.assertEqual(self.notification.email, self.EMAIL)
-        self.assertEqual(self.notification.address, self.EMAIL_LIST)
