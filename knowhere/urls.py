f5720dbec7aef2f570c6cafab91aea3b8a84f454
KingStrafeIV <bz.kalashnikov@gmail.com>
2015-10-14 16:50:35 -0500

    KW-38 Initial pass at installing auth

--- knowhere/urls.py
+++ knowhere/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', include('website.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls'))
 ]
