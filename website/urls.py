e3a61ebee0a1f6af7699e676f90d7a59ef119eeb
KingStrafeIV <bz.kalashnikov@gmail.com>
2015-10-21 16:27:21 -0500

    KW-8 Adding notification views

--- website/urls.py
+++ website/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^notification_zone/(?P<pk>[0-9]+)/$', views.notification_zone_detail, name='notification_zone_detail'),
     url(r'^group/new/$', views.group_new, name='group_new'),
     url(r'^group/(?P<pk>[0-9]+)/$', views.group_detail, name='group_detail'),
+    url(r'^notification/new/$', views.notification_new, name='notification_new'),
+    url(r'^notification/(?P<pk>[0-9]+)/$', views.notification_detail, name='notification_detail'),
 ]
