e313ed2a770aeed9bc91ce5bef7ed99ab28f282c
KingStrafeIV <bz.kalashnikov@gmail.com>
2015-11-23 22:03:18 -0600

    MAINT fixing tests

--- website/tests/test_views.py
+++ website/tests/test_views.py
@@ -12,22 +12,26 @@ class NotificationZoneViewTest(TestCase):
     group = Group()
 
     def test_calculation_of_latitude_and_longitude_on_creation(self):
-        self.given_a_logged_in_user()
+        self.given_a_logged_in_user_and_group()
         self.when_viewing_the_notification_zone_create_form()
         self.when_creating_a_notification_zone()
         self.then_latitude_and_longitude_are_calculated_and_saved()
 
-    def given_a_logged_in_user(self):
+    def given_a_logged_in_user_and_group(self):
         user = User.objects.create(username=self.USERNAME, is_active=True)
         user.set_password("password")
         user.save()
         self.client.login(username=self.USERNAME, password="password")
+
+        self.group = Group.objects.create(admin=user, name="TEST_GROUP")
+        self.group.users.add(user)
 
     def when_viewing_the_notification_zone_create_form(self):
         self.client.get("/notification_zone/new")
 
     def when_creating_a_notification_zone(self):
         self.response = self.client.post("/notification_zone/new/", {
+            "groups" : [self.group.id],
             "name" : "test_notification_zone",
             "address" : "6220 Culebra Rd",
             "city" : "San Antonio",
